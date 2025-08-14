ca60e96460bb979e0159f39419c6b6954bf68261
ToughTeddy <kstoeck99@outlook.com>
2025-08-14 09:47:18 -0700

    update iss_tracker.py
    added iss_close_check method that checks if iss is near my positions.

--- ISS_Tracker/iss_tracker.py
+++ ISS_Tracker/iss_tracker.py
@@ -10,11 +10,25 @@ def iss_position():
 
         iss_latitude = float(data["iss_position"]["latitude"])
         iss_longitude = float(data["iss_position"]["longitude"])
+        
     except:
         iss_latitude = 0
         iss_longitude = 0
 
     return iss_latitude, iss_longitude
 
-print(iss_position())
+# print(iss_position())
 
+def iss_close_check():
+    check = False
+
+    iss_lat, iss_long = iss_position()
+
+    if iss_lat != 0 and iss_long != 0:
+        if (me.MY_LAT - 5 <= iss_lat <= me.MY_LAT + 5
+                and me.MY_LONG - 5 <= iss_long <= me.MY_LONG + 5):
+            check = True
+
+    return check
+
+# print(iss_close_check())
